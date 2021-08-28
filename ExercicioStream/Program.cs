fe0bb05e47a669fa09dd9c35553cb74dbfb3cb69
tiagoalmeida48 <53452661+tiagoalmeida48@users.noreply.github.com>
2021-08-27 21:21:16 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioStream/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
